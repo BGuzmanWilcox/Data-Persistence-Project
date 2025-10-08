698faf2cc92a17560a05e56cf1e756f733afe18e
BGuzmanWilcox <skysku32887@gmail.com>
2025-10-08 18:34:51 -0500

    Updated muit scripts

---
+++ Assets/Scripts/UiMenuManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using TMPro;
+
+public class UiMenuManager : MonoBehaviour
+{
+
+    public TMP_InputField userName;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+
+        EditorApplication.EnterPlaymode();
+#else
+        Application.Quit();
+#endif
+
+    }
+    
+    public void UserName()
+    {
+        if (userName != null) 
+        { 
+        
+        }
+    }
+}
